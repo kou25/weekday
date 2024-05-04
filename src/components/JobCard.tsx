9c0fbb9efb3e1afcd90049788738793b7873b9b3
kou25 <koustav.manna98@gmail.com>
2024-05-05 02:45:55 +0530

    Add JobCard component and update Jobs component layout

---
+++ src/components/JobCard.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Box, Button, Paper, Typography } from "@mui/material";
+import { useState } from "react";
+export const JobCard = ({ job }: { job: any }) => {
+  const [expand, setExpand] = useState<boolean>(false);
+
+  const toggleExpand = () => {
+    setExpand(!expand);
+  };
+  return (
+    <Paper elevation={2} className="card">
+      <div className="company-info">
+        <Box mr={2}>
+          <img src={job.logoUrl} alt="company logo" />
+        </Box>
+        <Box>
+          <p className="company-name">{job.companyName}</p>
+          <p className="job-role">{job.jobRole}</p>
+          <small>{job.location}</small>
+        </Box>
+      </div>
+      <Box my={2}>
+        <Typography variant="body2" fontWeight={300}>
+          Estimated Salary: ₹{job.minJdSalary ?? 0} - {job.maxJdSalary}
+          {"  "}
+          LPA ✅
+        </Typography>
+      </Box>
+      <Box mb={1}>
+        <Typography variant="body1" fontWeight={500}>
+          Job description:
+        </Typography>
+      </Box>
+      <Box mb={2}>
+        {expand
+          ? job.jobDetailsFromCompany
+          : job.jobDetailsFromCompany.slice(0, 600) + "... "}
+        {job.jobDetailsFromCompany.length > 600 && (
+          <small id="expand" onClick={toggleExpand}>
+            {expand ? "Hide" : "Show more"}
+          </small>
+        )}
+      </Box>
+      <div className="info-container">
+        <h3>Minimum Experience</h3>
+        <p>{job.minExp ?? 0} years</p>
+      </div>
+      <Box mt={2}>
+        <Button
+          variant="contained"
+          color="primary"
+          fullWidth
+          disableElevation={true}
+          sx={{
+            backgroundColor: "rgb(85, 239, 196)",
+            color: "rgb(0, 0, 0)",
+            fontWeight: "500",
+            padding: "8px 18px",
+            borderRadius: "8px",
+            margin: "5px 0",
+            textTransform: "capitalize",
+            "&:hover": {
+              backgroundColor: "rgb(85, 239, 196)"
+            }
+          }}
+        >
+          ⚡ Easy Apply
+        </Button>
+      </Box>
+    </Paper>
+  );
+};
--- src/components/Jobs.tsx
+++ src/components/Jobs.tsx
@@ -1,26 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Grid, Paper, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import "./style/Jobs.css";
+import { JobCard } from "./JobCard";
 const Jobs = ({ data }: { data: any[] }) => {
   return (
     <Grid
       container
-      spacing={8}
-      columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+      spacing={6}
+      columnSpacing={{ xs: 1, sm: 2, md: 4 }}
       sx={{ marginTop: 2 }}
     >
       {data.map((job: any) => (
         <Grid item xs={4} key={job.jdUid}>
-          <Paper elevation={2} className="card">
-            <h3>{job.companyName}</h3>
-            <p>{job.jobRole}</p>
-            <small>{job.location}</small>
-            <Typography variant="body1">Job description:</Typography>
-            <p>{job.jobDetailsFromCompany}</p>
-
-            <p>Minimum Experience</p>
-            <p>{job.minExp ?? 0} years</p>
-          </Paper>
+          <JobCard job={job} />
         </Grid>
       ))}
     </Grid>
