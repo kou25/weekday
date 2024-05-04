42b8e547c5df2a99c919ecb9dffb7728f715353e
kou25 <koustav.manna98@gmail.com>
2024-05-04 22:53:15 +0530

    Add Navbar and WeekdayTabs components

---
+++ src/components/WeekdayTabs.tsx
@@ -0,0 +1,46 @@
+import { Tab, Tabs } from "@mui/material";
+import "./style/WeekdayTabs.css";
+
+type Props = {
+  selected: string;
+  onChange: (value: string) => void;
+};
+export const WeekdayTabs = ({ selected, onChange }: Props) => {
+  return (
+    <section className="weekdayTabs-container">
+      <Tabs
+        value={selected}
+        TabIndicatorProps={{
+          style: {
+            backgroundColor: "#3522e2",
+            height: "1px"
+          }
+        }}
+        sx={{
+          minHeight: "0px !important",
+          height: "40px"
+        }}
+        onChange={(event: React.SyntheticEvent, newValue: string) =>
+          onChange(newValue)
+        }
+      >
+        <Tab
+          value="applied"
+          label="Applied jobs"
+          disabled
+          sx={{
+            textTransform: "Capitalize"
+          }}
+        />
+        <Tab
+          value="search"
+          label="Search jobs"
+          sx={{
+            textTransform: "Capitalize",
+            color: "black !important"
+          }}
+        />
+      </Tabs>
+    </section>
+  );
+};
